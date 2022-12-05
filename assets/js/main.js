e5838df739fbb5322585063cb89a4988fd809958
RobertoBaiochi <robertombbaiochi@gmail.com>
2022-12-05 04:04:50 -0300

    refactor: changing the logic applied to functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
-function formulario() {
+(function () {
+  // Selecionando elementos da DOM
   const form = document.querySelector("#formulario");
   const login = form.querySelector("#login");
   const password = form.querySelector("#password");
@@ -7,182 +8,137 @@ function formulario() {
   const button = form.querySelector("#button");
   const small = form.querySelector("#erro");
 
-  const inputsValues = [login, password, confirmation, email];
-
+  /*
+      Deixando o primeiro input em foco assim que página é montada.
+    */
   login.focus();
 
+  /*
+      Tratamento dos dados ao evento de clique para envio das informações
+    */
   button.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    /* -------------  LOGIN --------------- */
+    if (!login.value.trim()) {
+      if (login.value.trim().length === 0) {
+        handleError(login, "Este campo é obrigatório!");
+        return;
+      }
+
+      if (login.value.trim().length < 5) {
+        handleError(login, "O login deve conter mais de 5 caracteres.");
+        return;
+      }
+
+      setCorrectValues(login);
+    }
+
+    if (!password.value.trim()) {
+      if (password.value.trim().length === 0) {
+        handleError(password, "Este campo é obrigatório!");
+        clearCorrectValues(confirmation);
+        handleError(confirmation, "As senhas devem ser iguais.");
+        clearInputValue(confirmation);
+        return;
+      }
+    }
+  });
+
+  /*
+      >>>> INPUT EVENTS <<<<
+    */
+
+  form.addEventListener("input", (event) => {
     event.preventDefault();
     const currentElement = event.target;
 
-    const filterInput = inputsValues.filter((input) =>
-      !!input.value.trim() === false
-        ? input.nextElementSibling.classList.add("erro")
-        : input.nextElementSibling.classList.remove("erro")
-    );
-
+    /* -------------  LOGIN --------------- */
 
     if (currentElement === login) {
-      login.classList.remove("correto");
+      // Sempre que for escrever um novo login, limpa o campo de valor correto
+      clearCorrectValues(login);
 
+      // Verifica se o campo de login está preenchido
       if (login.value.trim().length === 0) {
-        login.nextElementSibling.classList.add("erro");
-        login.nextElementSibling.innerHTML = "Este campo é obrigatório!";
-        login.value = "";
-        return;
-      }
-      if (login.value.trim().length < 5) {
-        login.nextElementSibling.classList.add("erro");
-        login.nextElementSibling.innerHTML =
-          "O login deve conter mais de 5 caracteres.";
+        handleError(login, "Este campo é obrigatório!");
+        clearInputValue();
         return;
       }
 
-      login.nextElementSibling.classList.remove("erro");
-      login.classList.add("correto");
+      // Verifica se o nome do usuário é maior que 5 caracteres.
+      if (login.value.trim().length < 5) {
+        handleError(login, "O login deve conter mais de 5 caracteres.");
+        return;
+      }
+
+      // Coloca classes de que o campo foi preenchido corretamente
+      setCorrectValues(login);
     }
+
+    /* -------------  PASSWORD --------------- */
 
     if (currentElement === password) {
-      password.classList.remove("correto");
+      clearCorrectValues(password);
 
       if (password.value.trim().length === 0) {
-        password.nextElementSibling.classList.add("erro");
-        password.nextElementSibling.innerHTML = "Este campo é obrigatório!";
-        password.value = "";
-        confirmation.value = "";
-        confirmation.nextElementSibling.classList.add("erro");
-        confirmation.nextElementSibling.innerHTML =
-          "As senhas devem ser iguais.";
-        confirmation.classList.remove("correto");
-        return;
-      }
-      password.nextElementSibling.classList.remove("erro");
-      if (password.value.trim().length < 8) {
-        password.nextElementSibling.classList.add("erro");
-        password.nextElementSibling.innerHTML =
-          "A senha deve conter mais de 8 caracteres.";
-        return;
-      }
-      if (confirmation.value.trim() !== password.value) {
-        confirmation.value = "";
-        password.classList.add("correto");
+        handleError(password, "Este campo é obrigatório!");
+        clearCorrectValues(confirmation);
+        handleError(confirmation, "As senhas devem ser iguais.");
+        clearInputValue(confirmation);
         return;
       }
 
-      password.nextElementSibling.classList.remove("erro");
-      password.classList.add("correto");
+      if (password.value.trim().length < 8) {
+        handleError(password, "A senha deve conter mais de 8 caracteres.");
+        clearCorrectValues(confirmation);
+        handleError(confirmation, "As senhas devem ser iguais.");
+        return;
+      }
+
+      if (password.value.trim() === confirmation.value.trim()) {
+        setCorrectValues(password);
+        setCorrectValues(confirmation);
+      }
+
+      setCorrectValues(password);
     }
+
+    /* -------------  CONFIRMATION --------------- */
 
     if (currentElement === confirmation) {
-      confirmation.classList.remove("correto");
+      clearCorrectValues(confirmation);
 
       if (confirmation.value.trim().length === 0) {
-        confirmation.nextElementSibling.classList.add("erro");
-        confirmation.nextElementSibling.innerHTML = "Este campo é obrigatório!";
-        confirmation.value = "";
-        return;
-      }
-      if (confirmation.value.trim() !== password.value) {
-        confirmation.nextElementSibling.classList.add("erro");
-        confirmation.nextElementSibling.innerHTML =
-          "As senhas devem ser iguais.";
-        confirmation.value = "";
-        return;
-      }
-      if (confirmation.value.trim().length < 8) {
-        confirmation.nextElementSibling.classList.add("erro");
-        confirmation.nextElementSibling.innerHTML =
-          "A senha deve conter mais de 8 caracteres.";
+        handleError(confirmation, "Este campo é obrigatório!");
+        clearInputValue(confirmation);
         return;
       }
 
-      confirmation.nextElementSibling.classList.remove("erro");
-      confirmation.classList.add("correto");
-    }
-
-
-  });
-
-  document.addEventListener("change", (event) => {
-    const currentElement = event.target;
-
-    if (currentElement === login) {
-      login.classList.remove("correto");
-
-      if (login.value.trim().length === 0) {
-        login.nextElementSibling.classList.add("erro");
-        login.nextElementSibling.innerHTML = "Este campo é obrigatório!";
-        login.value = "";
-        return;
-      }
-      if (login.value.trim().length < 5) {
-        login.nextElementSibling.classList.add("erro");
-        login.nextElementSibling.innerHTML =
-          "O login deve conter mais de 5 caracteres.";
+      if (confirmation.value.trim() !== password.value.trim()) {
+        handleError(confirmation, "As senhas devem ser iguais.");
         return;
       }
 
-      login.nextElementSibling.classList.remove("erro");
-      login.classList.add("correto");
-    }
-
-    if (currentElement === password) {
-      password.classList.remove("correto");
-
-      if (password.value.trim().length === 0) {
-        password.nextElementSibling.classList.add("erro");
-        password.nextElementSibling.innerHTML = "Este campo é obrigatório!";
-        password.value = "";
-        confirmation.value = "";
-        confirmation.nextElementSibling.classList.add("erro");
-        confirmation.nextElementSibling.innerHTML =
-          "As senhas devem ser iguais.";
-        confirmation.classList.remove("correto");
-        return;
-      }
-      password.nextElementSibling.classList.remove("erro");
-      if (password.value.trim().length < 8) {
-        password.nextElementSibling.classList.add("erro");
-        password.nextElementSibling.innerHTML =
-          "A senha deve conter mais de 8 caracteres.";
-        return;
-      }
-      if (confirmation.value.trim() !== password.value) {
-        confirmation.value = "";
-        password.classList.add("correto");
-        return;
-      }
-
-      password.nextElementSibling.classList.remove("erro");
-      password.classList.add("correto");
-    }
-
-    if (currentElement === confirmation) {
-      confirmation.classList.remove("correto");
-
-      if (confirmation.value.trim().length === 0) {
-        confirmation.nextElementSibling.classList.add("erro");
-        confirmation.nextElementSibling.innerHTML = "Este campo é obrigatório!";
-        confirmation.value = "";
-        return;
-      }
-      if (confirmation.value.trim() !== password.value) {
-        confirmation.nextElementSibling.classList.add("erro");
-        confirmation.nextElementSibling.innerHTML =
-          "As senhas devem ser iguais.";
-        confirmation.value = "";
-        return;
-      }
-      if (confirmation.value.trim().length < 8) {
-        confirmation.nextElementSibling.classList.add("erro");
-        confirmation.nextElementSibling.innerHTML =
-          "A senha deve conter mais de 8 caracteres.";
-        return;
-      }
-
-      confirmation.nextElementSibling.classList.remove("erro");
-      confirmation.classList.add("correto");
+      setCorrectValues(confirmation);
     }
   });
-}
-formulario();
+
+  function handleError(inputName, errorMessage) {
+    inputName.nextElementSibling.classList.add("erro");
+    inputName.nextElementSibling.innerHTML = errorMessage;
+  }
+
+  function setCorrectValues(inputName) {
+    inputName.nextElementSibling.classList.remove("erro");
+    inputName.classList.add("correto");
+  }
+
+  function clearCorrectValues(inputName) {
+    inputName.classList.remove("correto");
+  }
+
+  function clearInputValue(inputName) {
+    inputName.value = "";
+  }
+})();
